07fafd5b40ef079694c00877044d6c74a9a54e6f
santodev <dseosantos76@gmail.com>
2023-05-13 23:45:06 -0400

    :sparkles: [ADD] useState variables

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import '../styles/Meme.css';
+import memesData from '../memesData';
+import { useState } from 'react';
 
 const Meme = () => {
+    const [memeImage, setMemeImage] = useState(null);
+    const getMemeImage = () =>{
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const randomUrl = (memesArray[randomNumber].url);
+        setMemeImage(randomUrl)
+    }
     return (
         <form action="#" className='form'>
-            <input type="text" name="" id="" className='form--input-text'/>
-            <input type="text" name="" id="" className='form--input-text'/>
-            <input type="button" value="Get a new meme image  🖼" className=' white-font form--submit-button purple-background'/>
+            <section className='form--input-text-container'>
+                <input type="text" placeholder='Top text' name="" id="" className='form--input-text'/>
+                <input type="text" placeholder='Bottom text' name="" id="" className='form--input-text'/>
+            </section>
+            <input type="button" onClick={getMemeImage} value="Get a new meme image  🖼" className=' white-font form--submit-button purple-background'/>
+            <img src={memeImage} alt=""/>
         </form>
     );
 }
