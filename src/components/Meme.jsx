34c62a3cba86381c1ad041a2460b21e02aeac41c
santodev <dseosantos76@gmail.com>
2023-05-25 23:04:20 -0400

    :iphone: [CSS] responsive styles with CSS Grid
    And also i added the handleChange function to when someone storke on  an input  this will show the output on our memeImage

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,21 +4,55 @@ import memesData from '../memesData';
 import { useState } from 'react';
 
 const Meme = () => {
-    const [memeImage, setMemeImage] = useState(null);
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: null
+    });
+    const [ allMemeImages ] = useState(memesData);
+
     const getMemeImage = () =>{
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const randomUrl = (memesArray[randomNumber].url);
-        setMemeImage(randomUrl)
+        const randomUrl = memesArray[randomNumber].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: randomUrl
+        }))
+    }
+    function handleChange(event) {
+        const {name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
     return (
-        <form action="#" className='form'>
-            <section className='form--input-text-container'>
-                <input type="text" placeholder='Top text' name="" id="" className='form--input-text'/>
-                <input type="text" placeholder='Bottom text' name="" id="" className='form--input-text'/>
+        <form action="#">
+            <main className='form'>
+                <input 
+                    type="text" 
+                    placeholder='Top text' 
+                    value={meme.topText}
+                    name="topText" 
+                    className='form--input-text'
+                    onChange={handleChange}
+                    />
+                    <input 
+                    type="text" 
+                    placeholder='Bottom text' 
+                    name="bottomText" 
+                    className='form--input-text'
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                    />
+                <input type="button" onClick={getMemeImage} value="Get a new meme image  🖼" className=' white-font form--submit-button purple-background'/>
+            </main>
+            <section className='meme'>
+                <img className='meme--image' src={meme.randomImage} alt=""/>
+                <h2 className='meme--text top'>{meme.topText}</h2>
+                <h2 className='meme--text bottom'>{meme.bottomText}</h2>
             </section>
-            <input type="button" onClick={getMemeImage} value="Get a new meme image  🖼" className=' white-font form--submit-button purple-background'/>
-            <img src={memeImage} alt=""/>
         </form>
     );
 }
